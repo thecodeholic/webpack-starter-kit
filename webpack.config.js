3eefde75263b6b3142d75ca81b29d05bfba0d5b8
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2019-01-05 23:13:09 +0400

    Install html-webpack-plugin
    Add dist folder in gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: 'development',
   entry: [
     './src/app.js'
   ],
-  devtool: 'source-maps'
+  devtool: 'source-maps',
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
 }
