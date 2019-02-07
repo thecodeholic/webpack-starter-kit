6c3186b22043cfd5ada868d817b2c771164af0e1
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2019-02-07 20:17:16 +0400

    Update webpack.config.prod.js, remove dev config parameters

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,95 +2,95 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const webpack = require('webpack');
 const path = require('path');
 
-module.exports = {
-  mode: 'production',
-  entry: [
-    './src/app.js'
-  ],
-  optimization: {
-    minimizer: [
-      new OptimizeCSSAssetsPlugin()
-    ]
-  }
-  ,
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-      title: 'Webpack starter project',
-      template: path.resolve('./src/index.html')
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader"
-        ]
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      },
-      {
-        test: /\.(jpg|jpeg|gif|png|svg|webp)$/,
-        use: [
-          {
-            loader: "file-loader",
+module.exports = function (env, argv) {
+  return {
+    mode: 'production',
+    entry: [
+      './src/app.js'
+    ],
+    optimization: {
+      minimizer: [
+        new OptimizeCSSAssetsPlugin()
+      ]
+    }
+    ,
+    plugins: [
+      new CleanWebpackPlugin(['dist']),
+      new HtmlWebpackPlugin({
+        title: 'Webpack starter project',
+        template: path.resolve('./src/index.html')
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            "sass-loader"
+          ]
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
             options: {
-              outputPath: './images',
-              name: "[name].[ext]",
-            },
-          },
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              mozjpeg: {
-                progressive: false,
-                quality: 45
-              },
-              // optipng.enabled: false will disable optipng
-              optipng: {
-                enabled: true,
-              },
-              pngquant: {
-                quality: '65-90',
-                speed: 4
-              },
-              gifsicle: {
-                interlaced: true,
-                optimizationLevel: 3
-              },
-              // the webp option will enable WEBP
-              webp: {
-                quality: 20
-              }
+              presets: ['@babel/preset-env']
             }
-          },
-        ],
-      },
-      {
-        test: /\.html$/,
-        use: {
-          loader: 'html-loader',
-        }
-      },
-    ]
-  }
-};
+          }
+        },
+        {
+          test: /\.(jpg|jpeg|gif|png|svg|webp)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                outputPath: './images',
+                name: "[name].[ext]",
+              },
+            },
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                mozjpeg: {
+                  progressive: false,
+                  quality: 45
+                },
+                // optipng.enabled: false will disable optipng
+                optipng: {
+                  enabled: true,
+                },
+                pngquant: {
+                  quality: '65-90',
+                  speed: 4
+                },
+                gifsicle: {
+                  interlaced: true,
+                  optimizationLevel: 3
+                },
+                // the webp option will enable WEBP
+                webp: {
+                  quality: 20
+                }
+              }
+            },
+          ],
+        },
+        {
+          test: /\.html$/,
+          use: {
+            loader: 'html-loader',
+          }
+        },
+      ]
+    }
+  };
+}
