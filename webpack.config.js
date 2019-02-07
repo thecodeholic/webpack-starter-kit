215b945a65d124c24a63a3ccbe843d0b51716758
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2019-02-07 20:10:49 +0400

    Remove config parameters from webpack.config.js which is for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,92 +1,76 @@
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const webpack = require('webpack');
 const path = require('path');
 
-module.exports = {
-  mode: 'development',
-  entry: [
-    './src/app.js'
-  ],
-  watch: true,
-  watchOptions: {
-    aggregateTimeout: 300, // Process all changes which happened in this time into one rebuild
-    poll: 1000, // Check for changes every second,
-    ignored: /node_modules/,
-    // ignored: [
-    //   '**/*.scss', '/node_modules/'
-    // ]
-  },
-  devtool: 'source-maps',
-  devServer: {
-    contentBase: path.join(__dirname, 'src'),
-    watchContentBase: true,
-    hot: true,
-    open: true,
-    inline: true
-  },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-      title: 'Webpack starter project',
-      template: path.resolve('./src/index.html')
-    }),
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          "css-loader",
-          "sass-loader"
-        ]
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      },
-      {
-        test: /\.(jpg|jpeg|gif|png|svg|webp)$/,
-        use: [
-          {
-            loader: "file-loader",
+module.exports = function(){
+  return {
+    mode: 'development',
+    entry: [
+      './src/app.js'
+    ],
+    watch: true,
+    watchOptions: {
+      aggregateTimeout: 300, // Process all changes which happened in this time into one rebuild
+      poll: 1000, // Check for changes every second,
+      ignored: /node_modules/,
+      // ignored: [
+      //   '**/*.scss', '/node_modules/'
+      // ]
+    },
+    devtool: 'source-maps',
+    devServer: {
+      contentBase: path.join(__dirname, 'src'),
+      watchContentBase: true,
+      hot: true,
+      open: true,
+      inline: true
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Webpack starter project',
+        template: path.resolve('./src/index.html')
+      }),
+      new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            'style-loader',
+            "css-loader",
+            "sass-loader"
+          ]
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
             options: {
-              outputPath: './images',
-              name: "[name].[ext]",
-            },
-          },
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              mozjpeg: {
-                progressive: false,
-                quality: 10
-              }
+              presets: ['@babel/preset-env']
             }
-          },
-        ]
-      },
-      {
-        test: /\.html$/,
-        use: {
-          loader: 'html-loader',
-        }
-      },
-    ]
-  }
-};
+          }
+        },
+        {
+          test: /\.(jpg|jpeg|gif|png|svg|webp)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                outputPath: './images',
+                name: "[name].[ext]",
+              },
+            },
+          ]
+        },
+        {
+          test: /\.html$/,
+          use: {
+            loader: 'html-loader',
+          }
+        },
+      ]
+    }
+  };
+}
